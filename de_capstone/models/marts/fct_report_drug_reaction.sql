262befcd2aed1ce0c1a71123aa8a673be75a4071
Nastaran Kouhdareh <263947291+nkouhdareh@users.noreply.github.com>
2026-08-09 15:09:17 +0200

    refactor(dbt): join dim_reporter + dim_date in fct so lineage shows full star

--- de_capstone/models/marts/fct_report_drug_reaction.sql
+++ de_capstone/models/marts/fct_report_drug_reaction.sql
@@ -2,20 +2,21 @@
 
 with events as (
     select *,
-        {{ dbt_utils.generate_surrogate_key(['resolved_drug', 'rxcui', 'brand_name']) }} as drug_nk,
-        {{ dbt_utils.generate_surrogate_key(['reporter_qualification_code', 'reporter_type', 'occur_country']) }} as reporter_key
+        {{ dbt_utils.generate_surrogate_key(['resolved_drug', 'rxcui', 'brand_name']) }} as drug_nk
     from {{ ref('stg_drug_event') }}
 ),
 res as ( select drug_nk, canonical_drug_name, resolution_tier from {{ ref('int_drug_resolution') }} ),
 d_drug as ( select drug_key, drug_name from {{ ref('dim_drug') }} ),
-d_reaction as ( select reaction_key, reaction_pt from {{ ref('dim_reaction') }} )
+d_reaction as ( select reaction_key, reaction_pt from {{ ref('dim_reaction') }} ),
+d_reporter as ( select reporter_key, reporter_qualification_code, reporter_type, occur_country from {{ ref('dim_reporter') }} ),
+d_date as ( select date_key, full_date from {{ ref('dim_date') }} )
 select
     e.report_drug_reaction_key,
     e.safety_report_id,
     coalesce(dd.drug_key, -1) as drug_key,
     rk.reaction_key,
-    e.reporter_key,
-    cast(to_char(e.receive_date, 'YYYYMMDD') as integer) as receive_date_key,
+    dr.reporter_key,
+    dt.date_key as receive_date_key,
     e.receive_date,
     e.drug_characterization as drug_characterisation,
     r.resolution_tier as drug_resolution_tier,
@@ -26,6 +27,11 @@ select
     '{{ invocation_id }}' as _run_id,
     {{ dbt.current_timestamp() }} as _loaded_at
 from events e
-left join res r        on e.drug_nk = r.drug_nk
-left join d_drug dd     on r.canonical_drug_name = dd.drug_name
-left join d_reaction rk on e.reaction_pt = rk.reaction_pt
+left join res r         on e.drug_nk = r.drug_nk
+left join d_drug dd      on r.canonical_drug_name = dd.drug_name
+left join d_reaction rk  on e.reaction_pt = rk.reaction_pt
+left join d_reporter dr
+    on  equal_null(e.reporter_qualification_code, dr.reporter_qualification_code)
+    and equal_null(e.reporter_type,               dr.reporter_type)
+    and equal_null(e.occur_country,               dr.occur_country)
+left join d_date dt      on e.receive_date = dt.full_date
